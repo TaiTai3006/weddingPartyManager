33c17f3d49b5499909b42623c76fc6f54a2a9024
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2023-04-16 21:48:08 +0700

    fix height dashboard

--- src/view/Dashboard.java
+++ src/view/Dashboard.java
@@ -324,10 +324,10 @@ public class Dashboard extends javax.swing.JFrame {
             jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jDesktopPane1Layout.createSequentialGroup()
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 822, Short.MAX_VALUE))
+                .addGap(0, 820, Short.MAX_VALUE))
         );
 
-        jPanel1.add(jDesktopPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 100, 1170, -1));
+        jPanel1.add(jDesktopPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 100, 1170, 830));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
