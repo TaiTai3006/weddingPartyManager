93b559892a1f31f728fb9af2d7a59baf72c9be91
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2023-04-16 22:18:05 +0700

    fix height dashboard

--- src/view/Dashboard.java
+++ src/view/Dashboard.java
@@ -57,7 +57,7 @@ public class Dashboard extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         NhanVien = new javax.swing.JPanel();
         jLabel6 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
         DangXuat = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jDesktopPane1 = new javax.swing.JDesktopPane();
@@ -281,20 +281,20 @@ public class Dashboard extends javax.swing.JFrame {
 
         jPanel4.add(NhanVien, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 350, 270, 56));
 
-        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Group 65 (1).png"))); // NOI18N
-        jPanel4.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 460, -1, -1));
+        jLabel9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Group 1000001512.png"))); // NOI18N
+        jPanel4.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 470, -1, -1));
 
-        DangXuat.setFont(new java.awt.Font("Helvetica Neue", 1, 20)); // NOI18N
-        DangXuat.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Group 1000001511.png"))); // NOI18N
+        DangXuat.setFont(new java.awt.Font("Helvetica Neue", 1, 15)); // NOI18N
+        DangXuat.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/Logout.png"))); // NOI18N
         DangXuat.setText("Đăng xuất");
         DangXuat.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mousePressed(java.awt.event.MouseEvent evt) {
                 DangXuatMousePressed(evt);
             }
         });
-        jPanel4.add(DangXuat, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 790, -1, -1));
+        jPanel4.add(DangXuat, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 700, -1, -1));
 
-        jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 271, 850));
+        jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 271, 730));
 
         jPanel2.setBackground(new java.awt.Color(251, 247, 244));
 
@@ -327,10 +327,10 @@ public class Dashboard extends javax.swing.JFrame {
             jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jDesktopPane1Layout.createSequentialGroup()
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 790, Short.MAX_VALUE))
+                .addContainerGap(670, Short.MAX_VALUE))
         );
 
-        jPanel1.add(jDesktopPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 50, 1170, 800));
+        jPanel1.add(jDesktopPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 50, 1170, 680));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -340,7 +340,7 @@ public class Dashboard extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 731, Short.MAX_VALUE)
         );
 
         pack();
@@ -479,7 +479,7 @@ public class Dashboard extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel4;
